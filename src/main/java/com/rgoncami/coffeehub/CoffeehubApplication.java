ac37ccfedce2dc93c9c2d12685c2c6fa8789049b
raimiyashiro <dev.raimiyashiro@gmail.com>
2020-11-27 23:19:57 -0300

    project: add basic setup

---
+++ src/main/java/com/rgoncami/coffeehub/CoffeehubApplication.java
@@ -0,0 +1,13 @@
+package com.rgoncami.coffeehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rgoncami/coffeehub/CoffeehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgoncami.coffeehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
