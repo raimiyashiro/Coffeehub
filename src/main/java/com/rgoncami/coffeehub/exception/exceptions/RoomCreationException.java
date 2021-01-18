68aa4ac657129b99c54e1f8c95781d4935fdfe7d
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-01-18 10:16:53 -0300

    fix: added RoomCreationException.java

---
+++ src/main/java/com/rgoncami/coffeehub/exception/exceptions/RoomCreationException.java
@@ -0,0 +1,15 @@
+package com.rgoncami.coffeehub.exception.exceptions;
+
+import com.rgoncami.coffeehub.exception.enums.RoomError;
+import lombok.Getter;
+
+public class RoomCreationException extends RuntimeException {
+
+    @Getter
+    private String code;
+
+    public RoomCreationException(RoomError e) {
+        super(e.getMessage());
+        this.code = e.getCode();
+    }
+}
