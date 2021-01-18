336c14bd7c3f170df4527755600aafcf47abbd8b
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-01-18 11:53:42 -0300

    security: added basic security

---
+++ src/main/java/com/rgoncami/coffeehub/security/ApiSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.rgoncami.coffeehub.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .antMatchers("/", "index")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
