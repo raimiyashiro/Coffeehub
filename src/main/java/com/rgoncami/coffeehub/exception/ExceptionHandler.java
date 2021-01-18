2c152df3e371eeca7809a30ebb6511e66b224e3c
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-01-18 00:32:37 -0300

    feat: added UserCreationException to @ExceptionHandler

--- src/main/java/com/rgoncami/coffeehub/exception/ExceptionHandler.java
+++ src/main/java/com/rgoncami/coffeehub/exception/ExceptionHandler.java
@@ -1,7 +1,25 @@
 package com.rgoncami.coffeehub.exception;
 
+import com.rgoncami.coffeehub.exception.exceptions.UserCreationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.time.ZonedDateTime;
 
 @ControllerAdvice
 public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public ResponseEntity<ApiException> handleUserCreationException(
+            UserCreationException e) {
+
+        ApiException apiException = new ApiException(
+                e.getCode(),
+                e.getMessage(),
+                ZonedDateTime.now()
+        );
+
+        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+    }
 }
